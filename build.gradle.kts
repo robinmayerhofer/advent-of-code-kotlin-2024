bef7a8c2707b30bfeb187017a203e827ef8c213a
Robin Mayerhofer <32525368+robinmayerhofer@users.noreply.github.com>
2024-12-02 06:40:11 +0100

    Setup benchmarking

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,18 @@
+import kotlinx.benchmark.gradle.JvmBenchmarkTarget
+import org.jetbrains.kotlin.allopen.gradle.AllOpenExtension
+
 plugins {
     kotlin("jvm") version "2.0.21"
+    kotlin("plugin.allopen") version "2.0.20"
+    id("org.jetbrains.kotlinx.benchmark") version "0.4.12"
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-benchmark-runtime:0.4.12")
+}
+
+configure<AllOpenExtension> {
+    annotation("org.openjdk.jmh.annotations.State")
 }
 
 sourceSets {
@@ -11,5 +24,26 @@ sourceSets {
 tasks {
     wrapper {
         gradleVersion = "8.11"
+    }
+}
+
+benchmark {
+    configurations {
+        named("main") {
+            iterationTime = 5
+            iterationTimeUnit = "sec"
+
+        }
+        register("Day01") {
+            iterationTime = 5
+            iterationTimeUnit = "sec"
+            include("days.day01.Day01Benchmark")
+        }
+    }
+    targets {
+        register("main") {
+            this as JvmBenchmarkTarget
+            jmhVersion = "1.35"
+        }
     }
 }
