f3984803c982200dea5ecff1ab097801e32e02bf
Robin Mayerhofer <32525368+robinmayerhofer@users.noreply.github.com>
2024-12-02 06:20:09 +0100

    Day 2

--- src/main/kotlin/days/day02/Day02.kt
+++ src/main/kotlin/days/day02/Day02.kt
@@ -1,19 +1,36 @@
 package days.day02
 
-import utils.println
-import utils.readInput
-import utils.shouldLog
-import utils.testFile
+import utils.*
+import kotlin.math.abs
 
 fun main() {
     shouldLog = true
 
+    fun List<Int>.allIncreasing(): Boolean =
+        zipWithNext().all { it.first < it.second }
+
+    fun List<Int>.allDecreasing() : Boolean =
+        zipWithNext().all { it.first > it.second }
+
+    fun List<Int>.maxDiffEqualOrBelow(value: Int): Boolean =
+        zipWithNext().all { abs(it.first - it.second) <= value }
+
     fun part1(input: List<String>): Int =
-        input.size
+        input.count {
+            val numbers = it.findAllNumbers()
+            (numbers.allDecreasing() || numbers.allIncreasing()) && numbers.maxDiffEqualOrBelow(3)
+        }
 
     fun part2(input: List<String>): Int =
-        input.size
+        input.count {
+            val numbers = it.findAllNumbers()
+            numbers.indices.any { index ->
+                val sublist = numbers.toMutableList().apply { removeAt(index) }
+                (sublist.allDecreasing() || sublist.allIncreasing()) && sublist.maxDiffEqualOrBelow(3)
+            }
+        }
 
+    println("Part 1 Test")
     testFile(
         "Part 1 Test 1",
         "Day02_test",
@@ -23,16 +40,19 @@ fun main() {
         2,
         filterBlank = false,
     )
-    val input = readInput("Day24").filter(String::isNotBlank)
+    println("Part 1")
+    val input = readInput("Day02").filter(String::isNotBlank)
     part1(input).println()
 
-//    testFile(
-//        "Part 2 Test 1",
-//        "Day02_test",
-//        ::part2,
-//        1,
-//        filterBlank = false,
-//    )
-//    val input2 = readInput("Day02").filter(String::isNotBlank)
-//    part2(input2).println()
+    println("Part 2 Test")
+    testFile(
+        "Part 2 Test 1",
+        "Day02_test",
+        ::part2,
+        4,
+        filterBlank = false,
+    )
+    println("Part 2")
+    val input2 = readInput("Day02").filter(String::isNotBlank)
+    part2(input2).println()
 }
