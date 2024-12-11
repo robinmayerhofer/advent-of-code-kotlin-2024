11fc19505c2a337671bc97ae2743b0b82c676678
Robin Mayerhofer <32525368+robinmayerhofer@users.noreply.github.com>
2024-12-11 06:49:22 +0100

    Day 12 Prep

---
+++ src/main/kotlin/days/day12/Day12.kt
@@ -0,0 +1,50 @@
+package days.day12
+
+import utils.measure
+import utils.println
+import utils.readInput
+import utils.shouldLog
+import utils.testFile
+
+fun main() {
+    shouldLog = true
+
+    fun part1(input: List<String>): Int =
+        input.size
+
+    fun part2(input: List<String>): Int =
+        input.size
+
+    shouldLog = true
+    testFile(
+        "Part 1 Test 1",
+        "Day12_test",
+        {
+            part1(it)
+        },
+        2,
+        filterBlank = false,
+    )
+
+    shouldLog = false
+    println("Solving part 1")
+    measure {
+        val input = readInput("Day12").filter(String::isNotBlank)
+        part1(input)
+    }.println()
+
+//    shouldLog = true
+//    testFile(
+//        "Part 2 Test 1",
+//        "Day12_test",
+//        ::part2,
+//        1,
+//        filterBlank = false,
+//    )
+//    shouldLog = false
+//    println("Solving part 2")
+//    measure {
+//        val input = readInput("Day12").filter(String::isNotBlank)
+//        part2(input)
+//    }.println()
+}
