3588470f4bf8512857ab17e2e41bbd489d5727e0
Robin Mayerhofer <32525368+robinmayerhofer@users.noreply.github.com>
2024-12-22 14:28:46 +0100

    Improve

--- src/main/kotlin/days/day22speed/Day22Speed.kt
+++ src/main/kotlin/days/day22speed/Day22Speed.kt
@@ -5,32 +5,26 @@ import kotlin.io.path.readLines
 
 object Day22Speed {
 
-    @JvmStatic
-    private inline fun Long.next(): Long {
+    private fun Long.next(): Long {
         val step1 = prune(mix(this, this * 64))
         val step2 = prune(mix(step1, step1 / 32))
         val step3 = prune(mix(step2, step2 * 2048))
         return step3
     }
 
-    @JvmStatic
-    private inline fun mix(a: Long, b: Long) = a xor b
+    private fun mix(a: Long, b: Long) = a xor b
 
-    @JvmStatic
-    private inline fun prune(a: Long) = a % 16777216
-
-    @JvmStatic
-    private val secretCount = 2000
+    private fun prune(a: Long) = a % 16777216
 
     @JvmStatic
     fun part2(input: List<String>): Int {
-        val numberss = Array(input.size) { IntArray(secretCount) }
-        val diffs = Array(input.size) { IntArray(secretCount - 1) }
+        val numberss = Array(input.size) { IntArray(2000) }
+        val diffs = Array(input.size) { IntArray(1999) }
 
         for (i in input.indices) {
             val initial = input[i].toLong()
             var current = initial
-            for (j in 0 until secretCount) {
+            for (j in 0 until 2000) {
                 numberss[i][j] = (current % 10).toInt()
                 current = current.next()
                 if (j >= 1) {
@@ -39,20 +33,21 @@ object Day22Speed {
             }
         }
 
-        val final = IntArray(19 * 19 * 19 * 19)
+        val final = IntArray(130321)
         val size = input.size
         var ans = 0
-        val factor = 19 * 19 * 19
+        val factor = 6859
 
+        val cur = IntArray(130321)
         for (i in input.indices) {
-            val cur = IntArray(19 * 19 * 19 * 19)
+            cur.fill(0)
             for (j in 1995 downTo 0) {
                 val diff1 = diffs[i][j] + 9
                 val diff2 = diffs[i][j + 1] + 9
                 val diff3 = diffs[i][j + 2] + 9
                 val diff4 = diffs[i][j + 3] + 9
-                val index = diff1 * factor + diff2 * 19 * 19 + diff3 * 19 + diff4
-                cur[index] = (numberss[i][j + 4] % 10).toInt()
+                val index = diff1 * factor + diff2 * 361 + diff3 * 19 + diff4
+                cur[index] = numberss[i][j + 4]
             }
             for (index in cur.indices) {
                 final[index] += cur[index]
